5e1fcf9a388ce1b38cee3701438bd93f3837062d
José Domingo Delgado Alonso <jose.delgado.alonso.al@iespoligonosur.org>
2025-02-04 16:30:04 +0100

    separaciones para la consola

--- main.py
+++ main.py
@@ -4,39 +4,51 @@ from ejercicios import*
 # Obtener el cliente de DynamoDB
 dynamodb = obtener_conexion()
 
-# EJERCICIO 1 
+print("===============================================================================================\n")
 print("Ejercicio 1: Crear al menos 3 tablas con tres atributos cada una")
-# crear_tablas(dynamodb)
+crear_tablas(dynamodb)
 
-# Ejercicio 2: Crear tres registros encada tabla
-# crear_registros(dynamodb)
+print("===============================================================================================\n")
+print("Ejercicio 2: Crear tres registros encada tabla")
+crear_registros(dynamodb)
 
-# Ejercicio 3: Obtener un registro de cada tabla
-# obtener_registros(dynamodb)
+print("===============================================================================================\n")
+print("Ejercicio 3: Obtener un registro de cada tabla")
+obtener_registros(dynamodb)
 
-# Ejercicio 4: Actualizar un registro de cada tabla
-# actualizar_registros()
+print("===============================================================================================\n")
+print("Ejercicio 4: Actualizar un registro de cada tabla")
+actualizar_registros()
 
-# Ejercicio 5: Eliminar un registro de cada tabla 
-# eliminar_registro_comentarios()
-# eliminar_registro_cursos()
-# eliminar_registro_persona()
+print("===============================================================================================\n")
+print("Ejercicio 5: Eliminar un registro de cada tabla") 
+eliminar_registro_comentarios()
+eliminar_registro_cursos()
+eliminar_registro_persona()
 
-# Ejercicio 6: Obtener todos los registros de cada tabla
-# mostrar_registros('Comentario')
-# mostrar_registros('Persona')
-# mostrar_registros('Cursos')
+print("===============================================================================================")
+print("Ejercicio 6: Obtener todos los registros de cada tabla")
+mostrar_registros('Comentario')
+mostrar_registros('Persona')
+mostrar_registros('Cursos')
 
-# Ejercicio 7: Obtener una conjunto de registros de un filtrado de cada tabla
-# filtrar_registros(dynamodb)
+print("===============================================================================================\n")
+print("Ejercicio 7: Obtener una conjunto de registros de un filtrado de cada tabla")
+filtrar_registros(dynamodb)
 
-# Ejercicio 8: Realizar una eliminación condicional de cada tabla
-# eliminar_registros_condicionalmente(dynamodb)
+print("===============================================================================================\n")
+print("Ejercicio 8: Realizar una eliminación condicional de cada tabla")
+eliminar_registros_condicionalmente(dynamodb)
 
-# Ejercicio 9: Obtener un conjunto de datos a través de varios filtros aplicado en cada tabla
+print("===============================================================================================\n")
+print("Ejercicio 9: Obtener un conjunto de datos a través de varios filtros aplicado en cada tabla")
 filtrar_registros_complejos(dynamodb)
 
-# Ejercicio 10: Utilizar PartiQL statement en cada tabla
+print("===============================================================================================\n")
+print("Ejercicio 10: Utilizar PartiQL statement en cada tabla")
 ejecutar_partiql(dynamodb)
 
+print("===============================================================================================\n")
+print("Ejercicio 11: Creamos el backup para cada tabla")
 crear_backup(dynamodb)
+print("===============================================================================================\n")
